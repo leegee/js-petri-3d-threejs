f6ddf38f8c496ca09c9dfdf9c2a15104cacbf591
Lee Goddard <lee@leegoddard.net>
2014-10-24 17:39:52 +0100

    timing

--- app/main.js
+++ app/main.js
@@ -1,15 +1,12 @@
 define(["lib/Petri", "three"], function main (Petri, THREE) {
 
     new Petri({
-        fullscreen          : true,
-        screenWidth         : window.innerWidth,
-        screenHeight        : window.innerHeight,
-        // sporesAt            : [
-        //     [-0.5, -0.5, -0.5],
-        //     [0, -0.5, -0.5],
-        //     [0, 0, 0],
-        //     [0.5, 0.5, 0.5],
-        //     [0.5, 0.5, 0]
-        // ]
+        sporesAt            : [
+            [-0.5, -0.5, -0.5],
+            [0, -0.5, -0.5],
+            [0, 0, 0],
+            [0.5, 0.5, 0.5],
+            [0.5, 0.5, 0]
+        ]
     });
 });
